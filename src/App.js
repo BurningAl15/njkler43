f81005b958b0506ad9955cd1ebecb348d72724a6
Aldhair Vera <aldhairvera15@gmail.com>
2022-01-05 23:04:14 -0500

    fix: tests passed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,43 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input_content: '',
+      post_list: posts
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ input_content: e.target.value })
+    if (this.state.input_content.trim().length > 0) {
+      let clone = posts.filter((value) => (value.title.toLowerCase().includes(this.state.input_content) && value));
+      this.setState({ post_list: clone });
+    }
+    else if (this.state.input_content.length <= 0) {
+      this.setState({ post_list: posts });
+    }
+
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" onChange={this.handleChange} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {
+            this.state.post_list.map((value, index) => {
+              return (
+                <li key={index}>
+                  <a href={value.url}><img src={value.image} alt="AAA" /></a>
+                  <p>{value.title}</p>
+                </li>
+              )
+            })
+          }
         </ul>
       </div>
     )
